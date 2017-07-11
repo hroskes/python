4757fe9a49c38d2746ebcf60752f29529bd7b594
Heshy Roskes <jroskes1@jhu.edu>
2017-07-11 11:09:00 -0400

    saveas for MARCC

--- rootoverloads.py
+++ rootoverloads.py
@@ -37,6 +37,55 @@ def definegetitem(cls, getlistfunctionname, getitem=True, iter=True, len_=True, 
 definegetattr(ROOT.TDirectory, "Get")
 definegetattr(ROOT.RooWorkspace, "obj")
 
-definegetitem(ROOT.TCanvas, "GetListOfPrimitives")
+definegetitem(ROOT.TPad, "GetListOfPrimitives")
 definegetitem(ROOT.THStack, "GetHists")
 definegetitem(ROOT.TDirectory, "GetListOfKeys", furtheraction=lambda x: x.ReadObj())
+
+import getpass, socket
+if ("login-node" in socket.gethostname() or "compute" in socket.gethostname() or "bigmem" in socket.gethostname()) and getpass.getuser():
+    class __PlotCopier(object):
+        import os, subprocess
+        def __init__(self):
+            self.__tmpdir = None
+
+        def __del__(self):
+            if self.__tmpdir is not None:
+                import os, subprocess
+                subprocess.check_call(["rsync", "-azvP", os.path.join(self.tmpdir, "www", ""), "hroskes@lxplus.cern.ch:www/"])
+
+        @property
+        def tmpdir(self):
+            import tempfile
+            if self.__tmpdir is None: self.__tmpdir = tempfile.mkdtemp()
+            return self.__tmpdir
+
+        def marccfilename(self, filename):
+            import os
+            if "/www/" in filename:
+                filename = os.path.join(self.tmpdir, "www", filename.split("/www/", 1)[1].lstrip("/"))
+                self.mkdir_p(os.path.dirname(filename))
+            return filename
+
+        def mkdir_p(self, path):
+            """http://stackoverflow.com/a/600612/5228524"""
+            import errno, os
+            try:
+                os.makedirs(path)
+            except OSError as exc:
+                if exc.errno == errno.EEXIST and os.path.isdir(path):
+                    pass
+                else:
+                    raise
+
+    __plotcopier = __PlotCopier()
+
+    prevsaveas = ROOT.TPad.SaveAs
+    def SaveAs(self, filename="", *otherargs, **kwargs):
+        return prevsaveas(self, __plotcopier.marccfilename(filename), *otherargs, **kwargs)
+    ROOT.TPad.SaveAs = SaveAs
+
+    def CopyPlots():
+        global __plotcopier
+        __plotcopier = __PlotCopier()
+    assert not hasattr(ROOT, "CopyPlots")
+    ROOT.CopyPlots = CopyPlots
